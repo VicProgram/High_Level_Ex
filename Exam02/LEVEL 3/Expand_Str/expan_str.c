946389719b03bf5d11c7703ce1a5dfd077ebec05
Victor Abad Romero <vabad-ro@c2r11s1.42madrid.com>
2026-03-16 18:56:20 +0100

    one more

--- Exam02/LEVEL 3/Expand_Str/expan_str.c
+++ Exam02/LEVEL 3/Expand_Str/expan_str.c
@@ -1,46 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   epur_str.c                                         :+:      :+:    :+:   */
+/*   expan_str.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vabad-ro <vabad-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/16 14:24:19 by vabad-ro          #+#    #+#             */
-/*   Updated: 2026/03/16 18:46:55 by vabad-ro         ###   ########.fr       */
+/*   Created: 2026/03/16 18:38:42 by vabad-ro          #+#    #+#             */
+/*   Updated: 2026/03/16 18:56:06 by vabad-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main (int argc, char ** argv)
+int	main (int argc, char **argv)
 {
 	char	*str;
-	int	i = 0, j = 1;
+	int	i = 0, j = 0;
 
-	str = argv[1];
 	if (argc == 2)
 	{
+		str = argv[1];
 		while (str[i] == ' ' || str[i] == '\t')
 			i++;
-		while (str[i])
+		while(str[i])
 		{
-			if (str[i] != ' ' && str[i] != '\t')
-			{
-				if (!j)
-					write (1, " ", 1);
-				while (str[i] && (str[i] != ' ' && str[i] != '\t'))
-				{
-					write (1, &str[i], 1);
-					i++;
-				}
-				j = 0;
-			}
+			if ((str[i] == ' ' || str[i] == '\t'))
+				j = 1;
 			else
-				i++;
+			{
+				if (j == 1)
+					write (1, "   ", 3);
+				j = 0;
+				write (1, &str[i], 1);
+			}
+			i++;
 		}
 	}
-	else
-		i++;
 	write (1, "\n", 1);
 	return (0);
 }
