e843e889f367739628e9af268659702480a50d3e
Victor Abad Romero <vabad-ro@c2r11s1.42madrid.com>
2026-03-20 17:44:12 +0100

    correccion en floofill2

--- Exam02/LEVEL 4/Flood_Fill2/flood_fill2.c
+++ Exam02/LEVEL 4/Flood_Fill2/flood_fill2.c
@@ -1,45 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   flood_fill.c                                       :+:      :+:    :+:   */
+/*   flood_fill2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vabad-ro <vabad-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/19 12:45:24 by vabad-ro          #+#    #+#             */
-/*   Updated: 2026/03/20 17:43:59 by vabad-ro         ###   ########.fr       */
+/*   Created: 2026/03/20 17:14:48 by vabad-ro          #+#    #+#             */
+/*   Updated: 2026/03/20 17:43:56 by vabad-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "flood_fill.h"
+#include "flood_fill2.h"
 
-//recuerda hacer el .h bien!
 
-void	fill(char **tab, t_point size, t_point cur, char target)
+void	fill(char **tab,t_point size, t_point cur, char target)
 {
-	//comprobar si estamos dentro de tab
-	if (cur.x < 0 || cur.y < 0|| cur.x >= size.x || cur.y >= size.y)
+	if (cur.x < 0 || cur.y < 0 || cur.x >= size.x || cur.y >= size.y || tab[cur.y][cur.x] != target)
 		return;
-	//comprobar si es el caracter a cambiar
-	if (tab[cur.x][cur.y] != target)
+	if (tab[cur.y][cur.x] != target)
 		return;
-	//decir por que caracter se sustituye el target
 	tab[cur.y][cur.x] = 'F';
-
 	fill(tab, size, (t_point){cur.x - 1, cur.y}, target);
 	fill(tab, size, (t_point){cur.x + 1, cur.y}, target);
-	fill(tab, size, (t_point){cur.x, cur.y -1}, target);
-	fill(tab, size, (t_point){cur.x, cur.y +1}, target);
+	fill(tab, size, (t_point){cur.x, cur.y - 1}, target);
+	fill(tab, size, (t_point){cur.x, cur.y + 1}, target);
 }
 
-void  flood_fill(char **tab, t_point size, t_point begin)
-{	
-	//averiguar el caracter
-	char	target = tab[begin.y][begin.x];
-	//llamada normal a funcion de relleno
-	fill(tab, size, begin, target);
-}
+ void  flood_fill(char **tab, t_point size, t_point begin)
+ {
 
-///////////////////////////////
+	fill(tab, size, begin, tab[begin.y][begin.x]);
+ }
+
+
+ ///////////////////////////////
 
 #include <stdlib.h>
 #include <stdio.h>
