b7876324154c371ab384aa60437fc7570afd0b14
Victor Abad Romero <vabad-ro@c2r11s1.42madrid.com>
2026-03-23 19:15:18 +0100

    :)

--- Exam02/Attempt1/ft_itoa/ft_itoa.c
+++ Exam02/Attempt1/ft_itoa/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: vabad-ro <vabad-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 12:54:16 by vabad-ro          #+#    #+#             */
-/*   Updated: 2026/03/23 13:06:38 by vabad-ro         ###   ########.fr       */
+/*   Updated: 2026/03/23 19:14:33 by vabad-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ char	*ft_itoa(int nbr)
 		nb /= 10;
 		len++;
 	}
-
 	res = malloc(sizeof(char) * len +1);
 	if (!res)
 		return (NULL);
--- Exam02/Attempt1/ft_rrange/ft_rrange.c
+++ Exam02/Attempt1/ft_rrange/ft_rrange.c
@@ -6,12 +6,11 @@
 /*   By: vabad-ro <vabad-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 11:45:38 by vabad-ro          #+#    #+#             */
-/*   Updated: 2026/03/23 12:10:17 by vabad-ro         ###   ########.fr       */
+/*   Updated: 2026/03/23 19:15:13 by vabad-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int     *ft_rrange(int start, int end)
 {
@@ -28,19 +27,11 @@ int     *ft_rrange(int start, int end)
 	while (i < range)
 	{
 		res[i] = end;
-
 		if (end > start)
 			end--;
 		else if (start > end)
 			end++;
-		printf("%d", res[i]);
 		i++;
 	}
 	return (res);
-}
-
-int main(void)
-{
-	ft_rrange(0, -3);
-	return (0);
 }
